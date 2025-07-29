d73f00abc54b665b5388c7a6709d7b70100493af
Sphis-Sinco <sinconsistencia@gmail.com>
2025-07-29 16:17:31 -0500

    script file

--- source/product/scripts/Script.hx
+++ source/product/scripts/Script.hx
@@ -1,4 +1,20 @@
 package product.scripts;
 
-typedef ScriptFile = {}
+typedef ScriptFile =
+{
+	var logging:
+		{
+			var prefix:String;
+		};
+	var main_function:
+		{
+			var name:String;
+			var code:Array<
+				{
+					var function_name:String;
+					var args:Array<Dynamic>;
+				}>;
+		};
+}
+
 class ScriptManager {}
