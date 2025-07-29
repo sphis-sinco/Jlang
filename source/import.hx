8dce87846dbbabbdd59c7befaa10a6befe695cd3
Sphis-Sinco <sinconsistencia@gmail.com>
2025-07-29 16:25:18 -0500

    script parsing stuffs

--- source/import.hx
+++ source/import.hx
@@ -2,3 +2,4 @@ import flixel.*;
 import flixel.system.FlxModding;
 import product.*;
 import product.Init.InitManager;
+import product.scripts.*;
--- source/product/scripts/ScriptManager.hx
+++ source/product/scripts/ScriptManager.hx
@@ -1,3 +1,38 @@
 package product.scripts;
 
-class ScriptManager {}
+import haxe.Log;
+import haxe.PosInfos;
+
+class ScriptManager
+{
+	public static function parseScript(scriptFilePath:String)
+	{
+		final script:ScriptFile = cast Assets.getJsonFile(scriptFilePath, false);
+
+		var scriptTrace = function(v:Dynamic, ?infos:PosInfos)
+		{
+			var str = '[${script.logging.prefix ?? scriptFilePath}] $v';
+			#if js
+			if (js.Syntax.typeof(untyped console) != "undefined" && (untyped console).log != null)
+				(untyped console).log(str);
+			#elseif lua
+			untyped __define_feature__("use._hx_print", _hx_print(str));
+			#elseif sys
+			Sys.println(str);
+			#else
+			throw new haxe.exceptions.NotImplementedException();
+			#end
+		}
+
+		for (code in script.main_function.code)
+		{
+			switch (code.function_name.toLowerCase())
+			{
+				case 'print':
+					scriptTrace(code.args[0]);
+				default:
+					trace('UNKNOWN FUNCTION NAME WHILE PARSING: ${code.function_name}');
+			}
+		}
+	}
+}
