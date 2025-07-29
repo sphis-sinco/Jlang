73338d02309b3524f505f3a348eb733177d64bf0
Sphis-Sinco <sinconsistencia@gmail.com>
2025-07-29 17:23:19 -0500

    split these up

--- source/product/scripts/ScriptManager.hx
+++ source/product/scripts/ScriptManager.hx
@@ -6,36 +6,40 @@ class ScriptManager
 {
 	public static var scripts:Map<String, ScriptInfo> = [];
 
+	static var scriptinfoStuffs:ScriptInfo = {
+		variables: []
+	};
+
+	static var updateVariable = function(name:String, newValue:Dynamic) scriptinfoStuffs.variables.set(name, newValue);
+	static var getVariable = function(name:String) return scriptinfoStuffs.variables.get(name);
+
+	static var script:ScriptFile;
+
+	static function scriptTrace(v:Dynamic, ?scriptFilePath:String, ?infos:PosInfos)
+	{
+		var str = '[${script.logging.prefix ?? scriptFilePath}] $v';
+		#if js
+		if (js.Syntax.typeof(untyped console) != "undefined" && (untyped console).log != null)
+			(untyped console).log(str);
+		#elseif lua
+		untyped __define_feature__("use._hx_print", _hx_print(str));
+		#elseif sys
+		Sys.println(str);
+		#else
+		throw new haxe.exceptions.NotImplementedException();
+		#end
+	}
+
 	public static function parseScript(scriptFilePath:String)
 	{
-		var scriptinfoStuffs:ScriptInfo = {
+		scriptinfoStuffs = {
 			variables: []
 		};
 
 		if (!scripts.exists(scriptFilePath))
-		{
 			scripts.set(scriptFilePath, scriptinfoStuffs);
-		}
 
-		final script:ScriptFile = cast Assets.getScriptFile(scriptFilePath, false);
-
-		var updateVariable = function(name:String, newValue:Dynamic) scriptinfoStuffs.variables.set(name, newValue);
-		var getVariable = function(name:String) return scriptinfoStuffs.variables.get(name);
-
-		var scriptTrace = function(v:Dynamic, ?infos:PosInfos)
-		{
-			var str = '[${script.logging.prefix ?? scriptFilePath}] $v';
-			#if js
-			if (js.Syntax.typeof(untyped console) != "undefined" && (untyped console).log != null)
-				(untyped console).log(str);
-			#elseif lua
-			untyped __define_feature__("use._hx_print", _hx_print(str));
-			#elseif sys
-			Sys.println(str);
-			#else
-			throw new haxe.exceptions.NotImplementedException();
-			#end
-		}
+		script = cast Assets.getScriptFile(scriptFilePath, false);
 
 		for (code in script.main_function.code)
 		{
@@ -55,42 +59,52 @@ class ScriptManager
 			switch (code.function_name.toLowerCase())
 			{
 				case 'print':
-					switch (Std.string((args.get('arg1'))).toLowerCase())
-					{
-						case 'getvar':
-							final hasValueArg = args.get('arg2') != null;
-							if (hasValueArg)
-							{
-								scriptTrace(getVariable(args.get('arg2')));
-							}
-							else
-							{
-								scriptTrace('ERROR: MISSING "print getvar" ARG "value"');
-							}
-
-						default:
-							scriptTrace(args.get('arg1'));
-					}
+					printCommand(args, scriptFilePath);
 				case 'setvar':
-					final hasNameArg = args.get('arg1') != null;
-					final hasValueArg = args.get('arg2') != null;
-
-					if (hasNameArg && hasValueArg)
-					{
-						updateVariable(args.get('arg1'), args.get('arg2'));
-					}
-					else
-					{
-						if (!hasNameArg && !hasValueArg)
-							scriptTrace('ERROR: MISSING "setvar" ARGS "name" & "value"');
-						else if (!hasNameArg && hasValueArg)
-							scriptTrace('ERROR: MISSING "setvar" ARG "name"');
-						else if (hasNameArg && !hasValueArg)
-							scriptTrace('ERROR: MISSING "setvar" ARG "value"');
-					}
+					setVarCommand(args, scriptFilePath);
 				default:
-					scriptTrace('ERROR: UNKNOWN FUNCTION NAME WHILE PARSING: ${code.function_name}');
+					scriptTrace('ERROR: UNKNOWN FUNCTION NAME WHILE PARSING: ${code.function_name}', scriptFilePath);
 			}
+		}
+	}
+
+	public static function printCommand(args:Map<String, Dynamic>, scriptFilePath:String)
+	{
+		switch (Std.string((args.get('arg1'))).toLowerCase())
+		{
+			case 'getvar':
+				final hasValueArg = args.get('arg2') != null;
+				if (hasValueArg)
+				{
+					scriptTrace(getVariable(args.get('arg2')), scriptFilePath);
+				}
+				else
+				{
+					scriptTrace('ERROR: MISSING "print getvar" ARG "value"', scriptFilePath);
+				}
+
+			default:
+				scriptTrace(args.get('arg1'), scriptFilePath);
+		}
+	}
+
+	public static function setVarCommand(args:Map<String, Dynamic>, scriptFilePath:String)
+	{
+		final hasNameArg = args.get('arg1') != null;
+		final hasValueArg = args.get('arg2') != null;
+
+		if (hasNameArg && hasValueArg)
+		{
+			updateVariable(args.get('arg1'), args.get('arg2'));
+		}
+		else
+		{
+			if (!hasNameArg && !hasValueArg)
+				scriptTrace('ERROR: MISSING "setvar" ARGS "name" & "value"', scriptFilePath);
+			else if (!hasNameArg && hasValueArg)
+				scriptTrace('ERROR: MISSING "setvar" ARG "name"', scriptFilePath);
+			else if (hasNameArg && !hasValueArg)
+				scriptTrace('ERROR: MISSING "setvar" ARG "value"', scriptFilePath);
 		}
 	}
 }
