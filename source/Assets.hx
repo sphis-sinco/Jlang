9628e03cf7a58e967ad944f9fe4f894dc36f8222
Sphis-Sinco <sinconsistencia@gmail.com>
2025-07-29 15:53:39 -0500

    assets file to ez do shit

---
+++ source/Assets.hx
@@ -0,0 +1,50 @@
+class Assets
+{
+	public static var IMAGE_EXT:String = 'png';
+	public static var VIDEO_EXT:String = 'mp4'; // mayhaps
+	public static var SOUND_EXT:String = 'wav';
+
+	// file paths
+
+	public static function getPath(id:String)
+		return id;
+
+	public static function getAssetPath(id:String)
+		return getPath('assets/$id');
+
+	public static function getDataPath(id:String)
+		return getAssetPath('data/$id');
+
+	public static function getImagePath(id:String)
+		return getAssetPath('images/$id.$IMAGE_EXT');
+
+	public static function getMusicPath(id:String)
+		return getAssetPath('music/$id.$SOUND_EXT');
+
+	public static function getSoundPath(id:String)
+		return getAssetPath('sounds/$id.$SOUND_EXT');
+
+	// file content
+
+	public static function getFileTextContent(id:String, ?dataFolder:Bool = true)
+	{
+                var path = getAssetPath('$id');
+                if (dataFolder) path = getDataPath('$id');
+
+		return FlxModding.system.getText(path);
+	}
+
+	public static function getTextFile(id:String, ?dataFolder:Bool = true)
+		return getFileTextContent('$id.txt', dataFolder);
+
+	public static function getJsonFile(id:String, ?dataFolder:Bool = true)
+		return getFileTextContent('$id.json', dataFolder);
+
+        public static function getImage(id:String, ?imageFolder:Bool = true)
+        {
+                var path = getAssetPath('$id.$IMAGE_EXT');
+                if (imageFolder) path = getImagePath(id);
+
+                return FlxModding.system.getBitmapData(path);
+        }
+}
