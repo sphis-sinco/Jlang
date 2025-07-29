d5945f20ebb1de63a29e71e0dec6272decda28c5
Sphis-Sinco <sinconsistencia@gmail.com>
2025-07-29 16:09:07 -0500

    go to the runner

--- source/InitState.hx
+++ source/InitState.hx
@@ -1,12 +1,14 @@
 package;
 
-
 class InitState extends FlxState
 {
-        override function create() {
-                super.create();
+	override function create()
+	{
+		super.create();
 
 		InitManager.readInitFile(Assets.getJsonFile('init'));
 		trace('${ProductInfo.title} (${ProductInfo.version}) by ${ProductInfo.credits[0]} for JLang release ${ProductInfo.jlang_release}');
-        }
+
+		FlxG.switchState(() -> new Runner());
+	}
 }
