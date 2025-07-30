6c737fbb00fde070851e6cfa6db144c709153fdf
Sphis-Sinco <sinconsistencia@gmail.com>
2025-07-29 19:02:51 -0500

    gonna do input stuffs

--- source/Runner.hx
+++ source/Runner.hx
@@ -1,5 +1,8 @@
 package;
 
+import flixel.input.FlxInput;
+import flixel.input.keyboard.FlxKey;
+
 class Runner extends FlxState
 {
 	override public function create()
@@ -12,5 +15,47 @@ class Runner extends FlxState
 	override public function update(elapsed:Float)
 	{
 		super.update(elapsed);
+	}
+
+	var allowedKeys:String = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	var keyTime:Float = 0;
+
+	function handleKeyInput(elapsed:Float)
+	{
+		if (FlxG.keys.justPressed.ENTER) {}
+		else if (FlxG.keys.firstPressed() != FlxKey.NONE)
+		{
+			if (keyTime == 0 || keyTime > 0.3)
+			{
+				if (keyTime > 0.3)
+					keyTime = 0.25;
+				var keyPressed:Array<FlxInput<FlxKey>> = FlxG.keys.getIsDown();
+				for (i in keyPressed)
+				{
+					var key:FlxKey = i.ID;
+					switch (key)
+					{
+						case FlxKey.BACKSPACE:
+							// song = song.substring(0, song.length - 1);
+						case FlxKey.SPACE:
+							// song += " ";
+						default:
+							var keyName:String = Std.string(key);
+							if (allowedKeys.contains(keyName))
+							{
+								keyName = FlxG.keys.pressed.SHIFT ? keyName.toUpperCase() : keyName.toLowerCase();
+								// song += keyName;
+								// if (song.length >= 25)
+								//	song = song.substring(1);
+							}
+					}
+				}
+			}
+			keyTime += elapsed;
+		}
+		else
+		{
+			keyTime = 0;
+		}
 	}
 }
