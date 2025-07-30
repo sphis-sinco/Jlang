a0e83c9f3eca847434a9649a6f0d8f336aef67d7
Sphis-Sinco <sinconsistencia@gmail.com>
2025-07-30 16:29:42 -0500

    video "preloading" (ig)

--- source/InitState.hx
+++ source/InitState.hx
@@ -1,10 +1,24 @@
 package;
 
+import hxcodec.flixel.FlxVideo;
+
 class InitState extends FlxState
 {
 	override function create()
 	{
 		super.create();
+
+		#if sys
+		for (video in sys.FileSystem.readDirectory('assets/videos'))
+		{
+			trace('Hey a video: $video');
+			var videoObj:FlxVideo = new FlxVideo();
+			videoObj.cacheAsBitmap = true;
+			videoObj.volume = 0;
+			videoObj.play(Assets.getVideoPath(video));
+			videoObj.dispose();
+		}
+		#end
 
 		if (FlxModding.mods.length > 0)
 		{
